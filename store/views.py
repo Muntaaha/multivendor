2bb1e5bdebf8eee73c58bfdb61acf48a2ad96587
Muntaaha <muntaaha.rahman3001@gmail.com>
2020-06-14 23:44:01 +0600

    shipping-details

--- store/views.py
+++ store/views.py
@@ -143,7 +143,22 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['from']['total'])
+        order.transaction_id = transaction_id
 
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping = True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state']
+                zipcode = data['shipping']['zipcode']
+            )
     else:
         print('User is not logged in..')
     return JsonResponse('Payment Completed', safe=False)
