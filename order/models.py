b5eaedfbbc47aa3df8bd381fd4a26fde0d4982e5
Muntaaha <muntaaha.rahman3001@gmail.com>
2020-06-15 22:58:39 +0600

    Process-order-complete

--- order/models.py
+++ order/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 from store.models import *
 from product.models import *
 
@@ -34,15 +35,16 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    # price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
+    date_added = models.DateTimeField(default=datetime.now(), blank=True)
 
     def __str__(self):
         return str(self.id)
 
     @property
     def get_total(self):
-        total = self.price * self.quantity
+        total = self.product.product_price * self.quantity
         return total
 
 class ShippingDetails(models.Model):
@@ -55,4 +57,4 @@ class ShippingDetails(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return address
+        return self.address
--- store/views.py
+++ store/views.py
@@ -138,26 +138,28 @@ def updateItem(request):
 
 
 def processOrder(request):
-    transaction_id = datetime.datetime.now().timestamp()
+    transaction_id = datetime.now().timestamp()
+    data = json.loads(request.body)
 
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total = float(data['from']['total'])
+
+        total = float(data['form']['total'])
         order.transaction_id = transaction_id
 
         if total == order.get_cart_total:
             order.complete = True
         order.save()
 
-        if order.shipping = True:
-            ShippingAddress.objects.create(
+        if order.shipping == True:
+            ShippingDetails.objects.create(
                 customer=customer,
                 order=order,
                 address = data['shipping']['address'],
                 city = data['shipping']['city'],
-                state = data['shipping']['state']
-                zipcode = data['shipping']['zipcode']
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
             )
     else:
         print('User is not logged in..')
