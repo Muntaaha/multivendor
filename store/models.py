263ad665e338670f7c3c3acb7d62f55cfcd876cf
Muntaaha <muntaaha.rahman3001@gmail.com>
2020-06-18 00:00:37 +0600

    user-profile

--- store/models.py
+++ store/models.py
@@ -5,6 +5,9 @@ class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null= True, blank=True)
     name = models.CharField(max_length=200, null= True, blank=True)
     email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=200, null= True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
@@ -13,6 +16,9 @@ class Seller(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null= True, blank=True)
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=200, null= True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
